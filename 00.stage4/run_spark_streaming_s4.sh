221007efa3655036c7b4a7e246a8a741785616e8
freepsw <freepsw@gmail.com>
2017-10-15 20:54:23 +0900

    commit stage4 contents

---
+++ 00.stage4/run_spark_streaming_s4.sh
@@ -0,0 +1,15 @@
+spark-submit \
+	--class io.skiper.driver.Stage3StreamingDriver \
+	--master spark://localhost:7077 \
+	--name "demo-spark-analysis-s3" \
+	--deploy-mode client \
+	--driver-memory 1g \
+	--executor-memory 1g \
+	--total-executor-cores 2 \
+	--executor-cores 2 \
+	--conf "spark.driver.extraJavaOptions=-Dcom.sun.management.jmxremote \
+	        -Dcom.sun.management.jmxremote.port=8090 \
+					-Dcom.sun.management.jmxremote.authenticate=false \
+					-Dcom.sun.management.jmxremote.ssl=false \
+					-Djava.rmi.server.hostname=realtime"
+	../00.stage2/demo-streaming/target/demo-streaming-1.0-SNAPSHOT-jar-with-dependencies.jar
